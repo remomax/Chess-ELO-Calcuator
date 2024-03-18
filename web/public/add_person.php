e716150e4f0c6a08a6ae7ea182da21fb0a54cf60
Maximilian Schwarz <maximilian.schwarz100@gmail.com>
2024-03-18 12:42:15 +0100

    I've added back necessary files

--- web/public/add_person.php
+++ web/public/add_person.php
@@ -12,7 +12,6 @@ $_password = $_POST["password"];
 $_hash = password_hash($_password, PASSWORD_DEFAULT);
 $verify = password_verify($_password, $_hash);
 if ($verify = true) {
-
     $sql = sprintf("INSERT INTO person (age, lastname, firstname, elo, plz, hausnummer, street, email, games, username, password) 
             VALUES (%d, '%s', '%s', %d, '%s', '%s', '%s', '%s', %d, '%s', '%s')",
                 $_POST["age"],
@@ -27,10 +26,7 @@ if ($verify = true) {
                 $_POST["username"],
                 $_hash,
     );
-
-}
-elseif ($verify = false) {
-    echo "ERROR, Reload Page ";
+    $result = $connection->getConnection()->query($sql);
 }
 
 

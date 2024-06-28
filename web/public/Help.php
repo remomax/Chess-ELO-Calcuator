301abb35eb3e105daf0da2f8f9be5e14a549241d
Max <99677719+remomax@users.noreply.github.com>
2024-06-28 12:17:01 +0200

    Update Help.php
    Signed-off-by: Max <99677719+remomax@users.noreply.github.com>

--- web/public/Help.php
+++ web/public/Help.php
@@ -1,2 +1,2 @@
-<h1>If you need Help or an new ID send an email to with your Registert Email</h1>
-<h1><a href=mailto:"chesscalculatorhelp@gmail.com">chesscalculatorhelp@gmail.com</a></h1>
+//<h1>If you need Help or an new ID send an email to with your Registert Email</h1>
+//<h1><a href=mailto:"chesscalculatorhelp@gmail.com">chesscalculatorhelp@gmail.com</a></h1>

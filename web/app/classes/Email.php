70f80599a832346ed892619945d6d8843c49277d
Max <99677719+remomax@users.noreply.github.com>
2024-06-28 12:31:11 +0200

    Update Email.php
    Signed-off-by: Max <99677719+remomax@users.noreply.github.com>

--- web/app/classes/Email.php
+++ web/app/classes/Email.php
@@ -12,19 +12,19 @@ class Email
     {
         
         // Konfiguration
-        $mail_class['Mail'] = ''; //Hier die Email eintragen von sich selbst
-        $mail_class['Password'] = ''; //Hier das Email Password
-        $mail_class['isSMTP'] = ''; //Leer Lassen am besten
-        $mail_class['Host'] = ''; //Hier denn Host angeben
-        $mail_class['SMTPAuth'] = true; //Hier die SMTPAuth auf 'true' order 'false' setzen
-        $mail_class['Username'] = $mail_class['Mail'];
-        $mail_class['SMTPSecure'] ''; //
-        $mail_class['Port'] = ; //Port anbegen
+        $mail_class['Mail'] = ' '; //Hier die Email eintragen von sich selbst (string)
+        $mail_class['Password'] = ' '; //Hier das Email Password (string)
+        $mail_class['isSMTP'] = ''; //Leer Lassen am besten (string)
+        $mail_class['Host'] = ' '; //Hier denn Host angeben (string)
+        $mail_class['SMTPAuth'] = true; //Hier die SMTPAuth auf 'true' order 'false' setzen (bool)
+        $mail_class['Username'] = $mail_class['Mail']; (string)
+        $mail_class['SMTPSecure'] ' '; //Leer Lassen am besten (string)
+        $mail_class['Port'] = ; //Port anbegen (Int)
         // Empfänger
-        $mail_class['setFrom'] = $mail_class['Mail'], '' //Beim Zweiten einfach denn Namen angeben der angezeigt werden soll wie zb. Reiner Fischer
+        $mail_class['setFrom'] = $mail_class['Mail'], 'Hier Namen Von absender eintragen' //Beim Zweiten einfach denn Namen angeben der angezeigt werden soll wie zb. Reiner Fischer (string) (string)
 
         // Inhalt
-        $mail_class[isHTML] = true; //Das ambesten auf 'true' lassen das die Email in HTML Geschriben werden kann
+        $mail_class[isHTML] = true; //Das ambesten auf 'true' lassen das die Email in HTML Geschriben werden kann (bool)
 
         return new Email($mail_class);
     }

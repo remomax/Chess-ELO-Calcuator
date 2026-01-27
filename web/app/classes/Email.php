de2efee999fce0195ac11103ef124e9df1965426
Maximilan Schwarz <99677719+remomax@users.noreply.github.com>
2026-01-27 17:06:52 +0100

    Fixed comment
    Signed-off-by: Maximilan Schwarz <99677719+remomax@users.noreply.github.com>

--- web/app/classes/Email.php
+++ web/app/classes/Email.php
@@ -17,7 +17,7 @@ class Email
         $mail_class['isSMTP'] = ''; //Leer Lassen am besten (string)
         $mail_class['Host'] = ' '; //Hier denn Host angeben (string)
         $mail_class['SMTPAuth'] = true; //Hier die SMTPAuth auf 'true' order 'false' setzen (bool)
-        $mail_class['Username'] = $mail_class['Mail']; (string)
+        $mail_class['Username'] = $mail_class['Mail'];// (string)
         $mail_class['SMTPSecure'] ' '; //Leer Lassen am besten (string)
         $mail_class['Port'] = ; //Port anbegen (Int)
         // Empfänger
